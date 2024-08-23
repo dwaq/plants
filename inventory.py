d902d46118e0dba8a8a5bb3bdafbcbee90b36123
Dillon Nichols <ee.tinkerer@gmail.com>
2024-08-23 09:48:10 -0400

    general formatting of spreadsheet

---
+++ inventory.py
@@ -0,0 +1,29 @@
+import csv
+
+# Open the CSV file
+with open('Inventory.csv', 'r') as file:
+    reader = csv.reader(file)
+    
+    # Iterate over each row in the CSV file
+    for r, row in enumerate(reader):
+        # skip the header
+        if r==0:
+            continue
+
+        print(r, row)
+
+        name = row[0]
+
+        width = row[2].split("-")
+
+        minW = int(width[0])*12
+        maxW = int(width[1])*12
+        heightRange = row[3]
+        height = row[4]
+
+
+        print(name, minW, maxW, heightRange, height)
+
+        # exit after 1st row (testing)
+        if r==1:
+            exit()
